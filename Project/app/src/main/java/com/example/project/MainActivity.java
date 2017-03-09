762f1ab27bc376184485bec77c404325e70c0daf
xiedong <xiedong@xiedongdeMacBook-Pro.local>
2017-03-09 16:11:48 +0800

    合并master和dev

--- Project/app/src/main/java/com/example/project/MainActivity.java
+++ Project/app/src/main/java/com/example/project/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //变动
+        //合并master和dev
 
 
     }
