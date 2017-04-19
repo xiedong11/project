6f78bebd925d2b0bfd62dccabf0f962203f02fe7
xiedong <xiedong11@aliyun.com>
2017-04-19 17:38:13 +0800

    提交EditText自定义添加点击图片清空内容

--- Project/app/src/main/java/com/example/project/view/MyEditText.java
+++ Project/app/src/main/java/com/example/project/view/MyEditText.java
@@ -2,9 +2,11 @@ package com.example.project.view;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.provider.ContactsContract;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.example.project.R;
@@ -14,9 +16,11 @@ import com.example.project.R;
  */
 
 public class MyEditText extends LinearLayout {
+    private  int ImgResc;
     private  String mEditTextStr;
     private Context mContext;
     private EditText mEditText;
+    private ImageView mImageView;
 
     public MyEditText(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -24,6 +28,7 @@ public class MyEditText extends LinearLayout {
         mContext = context;
         TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.MyEditText);
         mEditTextStr = ta.getString(R.styleable.MyEditText_myText);
+        ImgResc = ta.getResourceId(R.styleable.MyEditText_myImgResc,R.mipmap.ic_launcher);
         ta.recycle();
 
         initView();
@@ -32,5 +37,15 @@ public class MyEditText extends LinearLayout {
     private void initView() {
         View.inflate(mContext,R.layout.my_edittext_view,this);
         mEditText = (EditText) findViewById(R.id.my_edittext);
+        mImageView = (ImageView) findViewById(R.id.img);
+
+        mEditText.setHint(mEditTextStr);
+        mImageView.setImageResource(ImgResc);
+        mImageView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEditText.setText("");
+            }
+        });
     }
 }
